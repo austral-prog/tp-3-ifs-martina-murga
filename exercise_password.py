a666b975e057a65ae868e794bc662c2163e4afbf
martina-murga <mmurga@mail.austral.edu.ar>
2026-04-14 02:37:35 -0300

    Update exercise_password.py

--- exercise_password.py
+++ exercise_password.py
@@ -23,4 +23,13 @@ def password():
         Contraseña muy corta
         Debe contener un numero
     """
-    pass
+    clave = input("")
+    tiene_largo = len(clave) >= 8
+    tiene_numero = ("0" in clave or "1" in clave or "2" in clave or "3" in clave or "4" in clave or "5" in clave or "6" in clave or "7" in clave or "8" in clave or "9" in clave)
+    if tiene_largo and tiene_numero:
+        print("Contraseña valida")
+    else:
+        if not tiene_largo:
+            print("Contraseña muy corta")
+        if not tiene_numero:
+            print("Debe contener un numero")
